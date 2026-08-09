2587943a93e0c3d106f00dfc8c747096890d2448
Yoga Sundaram <yogasundaram95@gmail.com>
2026-08-09 17:50:24 -0400

    Fix routing: use HashRouter for proper GitHub Pages SPA support

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import IndexBIE from "./pages/IndexBIE";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
   <TooltipProvider>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="bie" element={<IndexBIE />} />
+        <Route path="/bie" element={<IndexBIE />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </TooltipProvider>
 );
 
